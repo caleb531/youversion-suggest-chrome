f07e63782ad8062817e49dc40697f1c6146b1665
Caleb Evans <caleb@calebevans.me>
2017-09-09 21:53:11 -0700

    WIP: write raw markup for filter search results
    No business logic has been written yet.

--- src/scripts/components/app.js
+++ src/scripts/components/app.js
@@ -1,14 +1,28 @@
 import m from 'mithril';
 
+// The front-end application UI
 class AppComponent {
 
-  view() {
+  view(vnode) {
+    const state = vnode.state;
     return m('div.app', [
       m('header.app-header', [
         m('h1.app-title', 'YouVersion Suggest'),
         m('input[type=text].search-input', {
           placeholder: 'Type a book, chapter, verse, or keyword'
         })
+      ]),
+      m('div.search-results', [
+        // Search results from the reference filter (e.g. 1co13.3-7)
+        state.filterResults ?
+        m('ul.filter-results', [
+          state.filterResults.map((result) => {
+            return m('li.result.filter-result', [
+              m('div.result-title', result.title),
+              m('div.result-subtitle', result.subtitle)
+            ]);
+          })
+        ]) : null
       ])
     ]);
   }
