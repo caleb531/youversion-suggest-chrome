b64c83b5263c81ba47abe12ba194eb49403b127a
Caleb Evans <caleb@calebevans.me>
2017-09-09 19:46:15 -0700

    Create bare-bones app UI with header / search field

--- src/scripts/components/app.js
+++ src/scripts/components/app.js
@@ -3,7 +3,14 @@ import m from 'mithril';
 class AppComponent {
 
   view() {
-    return m('h1', 'YouVersion Suggest');
+    return m('div.app', [
+      m('header.app-header', [
+        m('h1.app-title', 'YouVersion Suggest'),
+        m('input[type=text].search-input', {
+          placeholder: 'Type a book, chapter, verse, or keyword'
+        })
+      ])
+    ]);
   }
 
 }
