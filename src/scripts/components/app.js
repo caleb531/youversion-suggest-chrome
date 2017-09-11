1cf0a151fd1bd9fa260f45112b1abc8eb49db498
Caleb Evans <caleb@calebevans.me>
2017-09-11 16:27:06 -0700

    Add magnifying glass icon to search field

--- src/scripts/components/app.js
+++ src/scripts/components/app.js
@@ -24,10 +24,15 @@ class AppComponent {
     return m('div.app', [
       m('header.app-header', [
         m('h1.app-title', 'YouVersion Suggest'),
-        m('input[type=text][autofocus].search-input', {
-          placeholder: 'Type a book, chapter, verse, or keyword',
-          oninput: this.triggerSearch.bind(this)
-        })
+        m('div.search-field-container', [
+          m('input[type=text][autofocus].search-field', {
+            placeholder: 'Type a book, chapter, verse, or keyword',
+            oninput: this.triggerSearch.bind(this)
+          }),
+          m('svg[viewBox="0 0 1792 1792"].search-field-icon', [
+            m('path[d="M1216 832q0-185-131.5-316.5t-316.5-131.5-316.5 131.5-131.5 316.5 131.5 316.5 316.5 131.5 316.5-131.5 131.5-316.5zm512 832q0 52-38 90t-90 38q-54 0-90-38l-343-342q-179 124-399 124-143 0-273.5-55.5t-225-150-150-225-55.5-273.5 55.5-273.5 150-225 225-150 273.5-55.5 273.5 55.5 225 150 150 225 55.5 273.5q0 220-124 399l343 343q37 37 37 90z"]')
+          ])
+        ])
       ]),
       m('div.search-results-container', [
         m('div.search-results-watermark', {
