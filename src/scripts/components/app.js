28aa8cf729bd30a2f1092c0be8f849f670997caf
Caleb Evans <caleb@calebevans.me>
2017-09-11 13:07:39 -0700

    Use one, unified results list; add "No Results" item

--- src/scripts/components/app.js
+++ src/scripts/components/app.js
@@ -8,11 +8,12 @@ class AppComponent {
     // Results for a "Filter by Reference" search
     this.referenceSearchResults = [];
     this.referenceSearch = new ReferenceSearch();
+    this.queryStr = '';
   }
 
   triggerSearch(inputEvent) {
-    const queryStr = inputEvent.target.value;
-    this.referenceSearch.search(queryStr).then((results) => {
+    this.queryStr = inputEvent.target.value;
+    this.referenceSearch.search(this.queryStr).then((results) => {
       this.referenceSearchResults = results;
       m.redraw();
     });
@@ -27,17 +28,21 @@ class AppComponent {
           oninput: this.triggerSearch.bind(this)
         })
       ]),
-      m('div.search-results-container', [
+      m('ol.search-results', [
+        this.queryStr !== '' && this.referenceSearchResults.length === 0 ?
+        m('li.search-result.null-result', [
+            m('div.search-result-title', 'No Results'),
+            m('div.search-result-subtitle', `No references matching \'${this.queryStr}\'`)
+        ]) : null,
         // Search results from the reference filter (e.g. 1co13.3-7)
-        this.referenceSearchResults.length > 0 ?
-        m('ol.reference-search-results', [
+        this.referenceSearchResults.length > 0 ? [
           this.referenceSearchResults.map((result) => {
             return m('li.search-result.reference-search-result', [
               m('div.search-result-title', result.title),
               m('div.search-result-subtitle', result.subtitle)
             ]);
           })
-        ]) : null
+        ] : null
       ])
     ]);
   }
