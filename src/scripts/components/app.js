8343ae4a258a3e06e2873d7e27f41bd1f782f616
Caleb Evans <caleb@calebevans.me>
2017-09-12 21:39:07 -0700

    Move search icon into its own component

--- src/scripts/components/app.js
+++ src/scripts/components/app.js
@@ -1,6 +1,7 @@
 import m from 'mithril';
 import classNames from 'classnames';
 import RefSearch from '../models/ref-search';
+import SearchIconComponent from './search-icon';
 
 // The front-end application UI
 class AppComponent {
@@ -82,9 +83,7 @@ class AppComponent {
             onkeydown: this.handleKeyboardNav.bind(this),
             oninput: (inputEvent) => this.triggerSearch(inputEvent.target.value)
           }),
-          m('svg[viewBox="0 0 1792 1792"].search-field-icon', [
-            m('path[d="M1216 832q0-185-131.5-316.5t-316.5-131.5-316.5 131.5-131.5 316.5 131.5 316.5 316.5 131.5 316.5-131.5 131.5-316.5zm512 832q0 52-38 90t-90 38q-54 0-90-38l-343-342q-179 124-399 124-143 0-273.5-55.5t-225-150-150-225-55.5-273.5 55.5-273.5 150-225 225-150 273.5-55.5 273.5 55.5 225 150 150 225 55.5 273.5q0 220-124 399l343 343q37 37 37 90z"]')
-          ])
+          m(SearchIconComponent)
         ])
       ]),
       m('div.search-results-container', {
