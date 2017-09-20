932ef18f15a1bbf2dd0e1de6059dca43738247ea
Caleb Evans <caleb@calebevans.me>
2017-09-19 20:09:48 -0700

    Move ContentSearcher HTML-parsing to separate function

--- src/scripts/models/content-searcher.js
+++ src/scripts/models/content-searcher.js
@@ -8,26 +8,32 @@ class ContentSearcher {
 
     let searchURL = `${this.constructor.baseSearchURL}`;
     return Core.getHTML(searchURL, {q: queryStr, version_id: 111}).then((html) => {
-
-      let $ = cheerio.load(html);
-      let $references = $('li.reference');
-
-      let results = [];
-      $references.each((r, reference) => {
-        let $reference = $(reference);
-        results.push(new RefResult({
-          title: $reference.find('h3').text().trim(),
-          subtitle: $reference.find('p').text().trim(),
-          uid: $reference.find('a').prop('href').match(Core.refUIDPattern)[0]
-        }));
-      });
-
+      let results = this.parseResults(html);
       if (results.length > 0) {
         return Promise.resolve(results);
       } else {
         return Promise.reject();
       }
     });
+
+  }
+
+  // Parse the content search results from the given HTML string
+  parseResults(html) {
+
+    let $ = cheerio.load(html);
+    let $references = $('li.reference');
+
+    let results = [];
+    $references.each((r, reference) => {
+      let $reference = $(reference);
+      results.push(new RefResult({
+        title: $reference.find('h3').text().trim(),
+        subtitle: $reference.find('p').text().trim(),
+        uid: $reference.find('a').prop('href').match(Core.refUIDPattern)[0]
+      }));
+    });
+    return results;
 
   }
 
