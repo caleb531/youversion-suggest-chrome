9f1ff42f098a04029805f67f6caa70d49028bd80
Caleb Evans <caleb@calebevans.me>
2017-09-23 11:53:26 -0700

    Fix bug where UI redrawing would break
    After completion of m.request, further interactions in the UI would
    cause Mithril redrawing to break completely. Duplicate elements would
    start appearing, mouse events wouldn't fire, essentially rendering the
    entire app unusable.
    
    Extensive debugging has narrowed down the root of the problem to
    m.request, and the fact that it (by default) redraws automatically upon
    request completion. However, because I trigger redraws manually
    and asynchronously in my search functions, I have no need for Mithril to
    perform this automatic redraw when the request completes. Fortunately,
    m.request supports a "background" parameter which, when set to true,
    will bypass this automatic redraw, thus fixing this redrawing bug
    completely.
    Source: <https://mithril.js.org/request.html>

--- src/scripts/models/core.js
+++ src/scripts/models/core.js
@@ -10,13 +10,19 @@ class Core {
       data: getParams,
       // By default, Mithril will try to parse response as JSON; return raw data
       // instead, since we are expecting HTML
-      deserialize: (content) => content
+      deserialize: (content) => content,
+      // Do not redraw views after request completion
+      background: true
     });
   }
 
   // Fetch any arbitrary JSON data via the given data file path
   static getJSON(dataFilePath) {
-    return m.request({url: dataFilePath});
+    return m.request({
+      url: dataFilePath,
+      // Again, no need to redraw upon completion
+      background: true
+    });
   }
 
   // Retrieve the Bible data for the current language (English for now)
