4dfeedc839c1f729bbaf543fdc743f7b3eceb362
Caleb Evans <caleb@calebevans.me>
2017-09-27 09:02:24 -0700

    Use new-style Options page
    The new-style (v2) options page was introduced in Chrome 40. See:
    <https://developer.chrome.com/extensions/optionsV2>

--- src/scripts/options.js
+++ src/scripts/options.js
@@ -3,15 +3,7 @@ import m from 'mithril';
 class OptionsComponent {
 
   view() {
-    return ('div.options-page', [
-      m('header.options-header', [
-        m('img.options-header-logo', {
-          src: 'icons/icon-square.svg',
-          alt: 'YouVersion Suggest'
-        }),
-        m('h1.options-page-title', 'YouVersion Suggest Options')
-      ])
-    ]);
+    return m('div.options-page');
   }
 
 }
