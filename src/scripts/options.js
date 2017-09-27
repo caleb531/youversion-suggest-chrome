8e942358f6ddb525e4eca6d9b1f1af7a5f46d418
Caleb Evans <caleb@calebevans.me>
2017-09-26 21:47:33 -0700

    Write markup for options page header

--- src/scripts/options.js
+++ src/scripts/options.js
@@ -3,8 +3,14 @@ import m from 'mithril';
 class OptionsComponent {
 
   view() {
-    return ('div.options', [
-      m('h1', 'YouVersion Suggest Options')
+    return ('div.options-page', [
+      m('header.options-header', [
+        m('img.options-header-logo', {
+          src: 'icons/icon-square.svg',
+          alt: 'YouVersion Suggest'
+        }),
+        m('h1.options-page-title', 'YouVersion Suggest Options')
+      ])
     ]);
   }
 
