caleb531/youversion-suggest-chrome

commit cc41dd2733cf18d80367e9fe2ff92ad19cfac71d

Add version field to Options page

src/scripts/models/core.js
@@ -25,20 +25,20 @@ class Core {
     });
   }
 
-  // Retrieve the Bible data for the current language (English for now)
-  static getBibleLanguageData() {
-    return this.getJSON('data/languages/language-eng.json');
+  // Retrieve the Bible data for the given language
+  static getBibleLanguageData(language) {
+    return this.getJSON(`data/bible/language-${language}.json`);
   }
 
   // Retrieve an object of YouVersion book IDs mapped to the number of chapters
   // in each
   static getBibleChapterData() {
-    return this.getJSON('data/languages/chapters.json');
+    return this.getJSON('data/bible/chapters.json');
   }
 
   // Retrieve the full list of supported languages
   static getLanguages() {
-    return Core.getJSON('data/languages/languages.json');
+    return Core.getJSON('data/bible/languages.json');
   }
 
   // Retrieve the raw user preferences without defaults merged in
@@ -67,9 +67,9 @@ class Core {
   }
 
   // Retrieve all relevant Bible data used by the extension
-  static getAllBibleData() {
+  static getAllBibleData(language) {
     return Promise.all([
-      this.getBibleLanguageData(),
+      this.getBibleLanguageData(language),
       this.getBibleChapterData()
     ]);
   }

src/scripts/options.js
@@ -5,15 +5,24 @@ import LoadingIconComponent from './components/loading-icon';
 class OptionsComponent {
 
   constructor() {
-    Promise.all([
-      Core.getPreferences(),
-      Core.getLanguages()
-    ])
-    .then(([preferences, languages]) => {
-      this.preferences = preferences;
-      this.languages = languages;
-      m.redraw();
-    });
+    Core.getPreferences()
+      .then((preferences) => {
+        this.preferences = preferences;
+        return Core.getLanguages();
+      })
+      .then((languages) => {
+        this.languages = languages;
+        return this.reloadVersions();
+      })
+      .then(() => m.redraw());
+  }
+
+  reloadVersions() {
+    return Core.getBibleLanguageData(this.preferences.language)
+      .then((bible) => {
+        this.versions = bible.versions;
+        this.defaultVersion = bible.default_version;
+      });
   }
 
   view() {
@@ -41,6 +50,23 @@ class OptionsComponent {
                   value: language.id,
                   selected: language.id === this.preferences.language
                 }, language.name);
+              })
+            ) : m('div.options-loading-icon-container', m(LoadingIconComponent))
+          ])
+        ]),
+
+        m('div.option-field.version-picker-container', [
+          m('.option-cell', (
+            m('label[for="version-picker"]', 'Version:')
+          )),
+          m('.option-cell', [
+            this.versions && this.preferences ?
+            m('select#version-picker',
+              this.versions.map((version) => {
+                return m('option', {
+                  value: version.id,
+                  selected: version.id === this.preferences.version
+                }, version.name);
               })
             ) : m('div.options-loading-icon-container', m(LoadingIconComponent))
           ])

commit 108a9d5fc9efd5bc1fd24ea70ba35d0c5e385972

Fix RefSearcher and make getAllBibleData() its ctor
The changes in cc41dd2 broke RefSearcher (because getAllBibleData() was
modified to accept a language parameter). getAllBibleData() was also
only being used in the RefSearcher, so it has been moved there as the
RefSearcher constructor.

src/scripts/models/core.js
@@ -66,14 +66,6 @@ class Core {
     });
   }
 
-  // Retrieve all relevant Bible data used by the extension
-  static getAllBibleData(language) {
-    return Promise.all([
-      this.getBibleLanguageData(language),
-      this.getBibleChapterData()
-    ]);
-  }
-
   // Change the query string to be in a consistent format
   static normalizeQueryStr(queryStr) {
     queryStr = queryStr.toLowerCase();

src/scripts/models/ref-searcher.js
@@ -5,6 +5,16 @@ import Reference from './reference.js';
 // Functions for searching the Bible by reference
 class RefSearcher {
 
+  constructor() {
+    this.allBibleData = Promise.all([
+      // Retrieve the Bible data for the currently-set language
+      Core.getPreferences().then((preferences) => {
+        return Core.getBibleLanguageData(preferences.language);
+      }),
+      Core.getBibleChapterData()
+    ]);
+  }
+
   // Search the Bible by reference name (e.g. "1 Corinthians 13:4-7"); return a
   // list of search results as JSON
   search(queryStr) {
@@ -19,7 +29,7 @@ class RefSearcher {
 
     // Ensure that bible/chapter data has loaded, then proceed to search for
     // Bible references matching the given query
-    return Core.getAllBibleData()
+    return this.allBibleData
       .then(([bible, chapters]) => {
         let matchingBooks = this.getBooksMatchingQuery(bible.books, query);
         let chosenVersion = this.chooseBestVersion(bible.versions, bible.default_version, query);