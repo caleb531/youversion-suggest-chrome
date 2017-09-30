d8ce929d4919fedf072c7498924a21c4cf705d20
Caleb Evans <caleb@calebevans.me>
2017-09-30 11:16:38 -0700

    Move popup search field into its own component

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,7 +1,8 @@
+import autoBind from 'auto-bind';
 import m from 'mithril';
 import classNames from 'classnames';
 import Searcher from './models/searcher';
-import SearchIconComponent from './search-icon';
+import SearchFieldComponent from './search-field';
 import LoadingIconComponent from './loading-icon';
 
 // The extension popup UI
@@ -11,53 +12,7 @@ class PopupComponent {
     // Initialize a new Searcher object, making sure to redraw whenever results
     // are updated
     this.searcher = new Searcher({onUpdateSearchStatus: () => m.redraw()});
-    this.bindAllMethods();
-  }
-
-  // Ensure that `this` is always bound to the class instance for all class
-  // methods, no matter how they're called
-  bindAllMethods() {
-    Object.getOwnPropertyNames(this.constructor.prototype).forEach((methodName) => {
-      if (methodName !== 'constructor') {
-        this.constructor.prototype[methodName] = this.constructor.prototype[methodName].bind(this);
-      }
-    });
-  }
-
-  // Handle keyboard shortcuts for navigating results
-  handleKeyboardNav(keydownEvent) {
-
-    let keyCode = keydownEvent.keyCode;
-    // Do not proceed if no results are selected
-    if (this.searcher.results.length === 0) {
-      // Prevent Mithril from redrawing for irrelevant keydown events
-      keydownEvent.redraw = false;
-      return;
-    }
-
-    if (keyCode === 13) {
-      // On enter key, action selected result (by default, view the reference)
-      this.searcher.getSelectedResult().runDefaultAction();
-      keydownEvent.preventDefault();
-      keydownEvent.redraw = false;
-    } else if (keyCode === 40) {
-      // On down arrow, select next result
-      this.searcher.selectNextResult();
-      keydownEvent.preventDefault();
-    } else if (keyCode === 38) {
-      // On up arrow, select previous result
-      this.searcher.selectPrevResult();
-      keydownEvent.preventDefault();
-    } else {
-      keydownEvent.redraw = false;
-    }
-
-  }
-
-  // Run the main search function as soon as the search field contents change
-  triggerSearch(inputEvent) {
-    this.searcher.search(inputEvent.target.value);
-    inputEvent.redraw = false;
+    autoBind(this);
   }
 
   // Get the index of a search result DOM element via its data-index attribute
@@ -137,15 +92,11 @@ class PopupComponent {
     return m('div.popup', [
       m('header.popup-header', [
         m('h1.popup-title', 'YouVersion Suggest'),
-        m('div.search-field-container', [
-          m('input[type=text][autofocus].search-field', {
-            placeholder: 'Type a book, chapter, verse, or keyword',
-            value: this.searcher.queryStr,
-            onkeydown: this.handleKeyboardNav,
-            oninput: this.triggerSearch
-          }),
-          m(SearchIconComponent)
-        ])
+        m(SearchFieldComponent, {
+          searcher: this.searcher,
+          autofocus: true,
+          placeholder: 'Type a book, chapter, verse, or keyword'
+        })
       ]),
 
       m('div.popup-content', [
---
+++ src/scripts/search-field.js
@@ -0,0 +1,64 @@
+import autoBind from 'auto-bind';
+import m from 'mithril';
+import SearchIconComponent from './search-icon';
+
+// A generic autocomplete-based search field
+class SearchFieldComponent {
+
+  constructor(vnode) {
+    this.searcher = vnode.attrs.searcher;
+    autoBind(this);
+  }
+
+  // Handle keyboard shortcuts for navigating results
+  handleKeyboardNav(keydownEvent) {
+
+    let keyCode = keydownEvent.keyCode;
+    // Do not proceed if no results are selected
+    if (this.searcher.results.length === 0) {
+      // Prevent Mithril from redrawing for irrelevant keydown events
+      keydownEvent.redraw = false;
+      return;
+    }
+
+    if (keyCode === 13) {
+      // On enter key, action selected result (by default, view the reference)
+      this.searcher.getSelectedResult().runDefaultAction();
+      keydownEvent.preventDefault();
+      keydownEvent.redraw = false;
+    } else if (keyCode === 40) {
+      // On down arrow, select next result
+      this.searcher.selectNextResult();
+      keydownEvent.preventDefault();
+    } else if (keyCode === 38) {
+      // On up arrow, select previous result
+      this.searcher.selectPrevResult();
+      keydownEvent.preventDefault();
+    } else {
+      keydownEvent.redraw = false;
+    }
+
+  }
+
+  // Run the main search function as soon as the search field contents change
+  triggerSearch(inputEvent) {
+    this.searcher.search(inputEvent.target.value);
+    inputEvent.redraw = false;
+  }
+
+  view(vnode) {
+    return m('div.search-field-container', [
+      m('input[type=text].search-field', {
+        autofocus: vnode.attrs.autofocus,
+        placeholder: vnode.attrs.placeholder,
+        value: this.searcher.queryStr,
+        onkeydown: this.handleKeyboardNav,
+        oninput: this.triggerSearch
+      }),
+      m(SearchIconComponent)
+    ]);
+  }
+
+}
+
+export default SearchFieldComponent;
