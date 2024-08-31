e761c4580ffa96deff80120cdb31a852cd7f4b9b
Caleb Evans <caleb@calebevans.me>
2024-08-31 09:33:41 -0700

    Reorganize project to fit Vite conventions
    HTML entry points should be at the project root.

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,12 @@ import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: 'src',
-  publicDir: '../public',
   build: {
-    outDir: '../dist',
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
       input: {
-        popup: fileURLToPath(new URL('./src/popup.html', import.meta.url)),
-        options: fileURLToPath(new URL('./src/options.html', import.meta.url)),
+        popup: fileURLToPath(new URL('./popup.html', import.meta.url)),
+        options: fileURLToPath(new URL('./options.html', import.meta.url)),
       },
       // Generate youversion-suggest chunk separately from rest of project
       output: {
